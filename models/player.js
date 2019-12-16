8f759b377ba76050d756fa467f768f8e7b5d969c
Alex Rocha <rocha8524@gmail.com>
2019-12-16 11:44:40 -0500

    Reorganizing files

--- models/player.js
+++ models/player.js
@@ -1,11 +1,10 @@
-module.exports = function(sequelize, DataTypes) {
-    var Player = sequelize.define("Player", {
-      player_name: DataTypes.STRING,
-      image: DataTypes.STRING,
-      position: DataTypes.STRING,
-      dateOfBirth: DataTypes.STRING,
-      nationality: DataTypes.STRING,
-    });
-    return Player;
-  };
-  
+module.exports = function (sequelize, DataTypes) {
+  var Player = sequelize.define("Player", {
+    player_name: DataTypes.STRING,
+    image: DataTypes.STRING,
+    position: DataTypes.STRING,
+    dateOfBirth: DataTypes.STRING,
+    nationality: DataTypes.STRING,
+  });
+  return Player;
+};
