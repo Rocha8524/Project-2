edd518efbfbde8a50ddf70f0d3ea023e91cc0b81
Alex Rocha <rocha8524@gmail.com>
2019-12-18 16:45:44 -0500

    Player data in player page

--- database/best-players.js
+++ database/best-players.js
@@ -167,7 +167,15 @@ var players = [
   }
 ];
 
-module.exports = players;
+var tr;
+for (var i = 0; i < players.length; i++) {
+  tr = $("<tr/>");
+  tr.append("<td>" + players[i].name + "</td>");
+  tr.append("<td>" + players[i].position + "</td>");
+  tr.append("<td>" + players[i].birthday + "</td>");
+  tr.append("<td>" + players[i].nationality + "</td>");
+  $("table").append(tr);
+}
 /*
 var dom = {
   $data: $("#data"),
