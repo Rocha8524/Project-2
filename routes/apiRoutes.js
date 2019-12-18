46aca087afb5e2a869edea21c28a27fecd419235
Alex Rocha <rocha8524@gmail.com>
2019-12-17 20:58:47 -0500

    Fixing players

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,16 @@
-var db = require("../models");
+// Create variables for dependencies
+var express = require("express");
+var router = express.Router();
+var database = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/best-players", function(request, response) {
-    db.bestPlayers.findAll({}).then(function(bestPlayers) {
+  app.get("/api/players", function(request, response) {
+    database.bestPlayers.findAll({}).then(function(bestPlayers) {
       response.json(bestPlayers);
     });
   });
 };
+
+// Export routes for server.js to use.
+module.exports = router;
